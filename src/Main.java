6eae841d28cc66b3f956514c52b8055584fe4494
lom <lom@ssvet.info>
2020-10-07 12:05:45 +0700

    comparing

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+
 public class Main {
     public static void main(String[] args){
 
@@ -15,6 +17,28 @@ public class Main {
 
         // lambda short version
         RunnableForLambdas.doSomething(() -> System.out.println("Runnable with short version lambda"));
+
+
+        // comparing Persons
+        Person person1 = new Person(1L,"first");
+        Person person2 = new Person(2L, "second");
+        //
+        Comparator<Person> compareById = new Comparator<Person>() {
+            @Override
+            public int compare(Person person1, Person person2) {
+                return person1.getId().compareTo(person2.getId());
+            }
+        };
+        int result = compareById.compare(person1, person2);
+        System.out.println(result);
+
+        Comparator<Person> compareByIdLambda = (person11, person21) -> person11.getId().compareTo(person21.getId());
+        int result2 = compareByIdLambda.compare(person1, person2);
+        System.out.println(result2);
+
+        Comparator<Person> compareShort = Comparator.comparing(Person::getId);
+        int result3 = compareShort.compare(person1, person2);
+        System.out.println(result3);
 
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,26 @@
+
+public class Person {
+    private Long id;
+    private String name;
+
+    Person(){
+
+    }
+    Person(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+}
